d6a5390c72942e5ea894cc325e9cf88a064f790b
deklerkwayne101010-bit <deklerkwayne101010@gmail.com>
2025-10-15 11:19:46 +0200

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -143,7 +143,7 @@ export default function LoginPage() {
 
           <div className="mt-8 text-center">
             <p className="text-slate-600">
-              Don't have an account?{' '}
+              Don&apos;t have an account?{' '}
               <Link
                 href="/register"
                 className="text-blue-600 hover:text-blue-700 font-semibold"
