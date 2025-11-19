42caa85b9125db2827f277b7271bfa4c0896ed04
deklerkwayne101010-bit <deklerkwayne101010@gmail.com>
2025-11-19 14:14:42 +0200

    feat: add second Renderform iframe to templates page

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -7,9 +7,16 @@ export default function TemplatesPage() {
   return (
     <ProtectedRoute>
       <DashboardLayout>
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto space-y-6">
           <iframe
             src="https://renderform.io/share/live-preview/?i=slow-ponies-trot-fiercely-1105"
+            width="100%"
+            height="500px"
+            frameBorder="0"
+          />
+
+          <iframe
+            src="https://renderform.io/share/live-preview/?i=purple-frogs-howl-smoothly-1437"
             width="100%"
             height="500px"
             frameBorder="0"
